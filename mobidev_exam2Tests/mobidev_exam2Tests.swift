dac78ab8a632d14910258db51c3e2ea4bc9583bb
pidalamatteo <pidalamatteo@gmail.com>
2025-09-22 20:08:44 +0200

    Add files via upload

---
+++ mobidev_exam2Tests/mobidev_exam2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  mobidev_exam2Tests.swift
+//  mobidev_exam2Tests
+//
+//  Created by Matteo on 09/09/25.
+//
+
+import Testing
+@testable import mobidev_exam2
+
+struct mobidev_exam2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
